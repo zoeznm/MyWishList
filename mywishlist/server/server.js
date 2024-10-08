d7ce9026060a6b4c74d3108587fe236bacb63f7f
zoeznm <bomikim046@gmail.com>
2024-10-08 14:26:16 +0900

    server/server.js 저장

--- mywishlist/server/server.js
+++ mywishlist/server/server.js
@@ -2,42 +2,24 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// 정적 파일 경로 설정
-app.use(express.static(path.join(__dirname, "../client/public"))); 
+// 정적 파일 경로 설정 (React 빌드 파일 제공)
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-// 뷰 엔진 설정
+// 뷰 엔진 설정 (만약 EJS도 같이 사용하려면 유지)
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../client/views")); 
+app.set("views", path.join(__dirname, "../client/views"));
 
-// 기본 라우트 설정
-app.get("/", (req, res) => {
-  res.render("main"); // client/views/main.ejs
+// API 또는 기타 서버 사이드 라우트
+app.get("/api/some-endpoint", (req, res) => {
+  res.json({ message: "This is an API response" });
 });
 
-app.get("/main", (req, res) => {
-  res.render("main"); // client/views/main.ejs
+// React 앱을 서빙하는 기본 라우트 (모든 경로가 index.html로 리다이렉트됨)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-app.get("/top", (req, res) => {
-  res.render("top"); // client/views/top.ejs
-});
-
-app.get("/bottom", (req, res)=> {
-  res.render("bottom");
-});
-
-app.get("/bag", (req, res)=> {
-  res.render("bag");
-});
-
-app.get("/shoes", (req, res)=> {
-  res.render("shoes");
-});
-
-app.get("/hat", (req, res)=> {
-  res.render("hat");
-});
-
+// 서버 시작
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
