e1e53822990c577f7b64464ae962c6cbf47a9415
zoeznm <bomikim046@gmail.com>
2024-10-09 22:35:50 +0900

    신발 라우팅 성공

---
+++ mywishlist/client/src/pages/shoes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/shoes.css"; 
+import "../scripts/shoes.js";
+
+const Shoes = () => {
+  return(
+    <h1>
+      여기는 신발페이지 입니다.
+    </h1>
+  );
+};
+
+export default Shoes;
