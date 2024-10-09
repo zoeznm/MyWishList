eab666e5dd7a67ecfdd3337c66c60d666ed115f3
zoeznm <bomikim046@gmail.com>
2024-10-09 22:35:15 +0900

    가방 라우팅 성공

---
+++ mywishlist/client/src/pages/bag.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/bag.css"; 
+import "../scripts/bag.js";
+
+const Bag = () => {
+  return(
+    <h1>
+      여기는 가방페이지 입니다.
+    </h1>
+  );
+};
+
+export default Bag;
