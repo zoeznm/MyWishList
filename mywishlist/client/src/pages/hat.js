8d826d85beeff44ae857fb7b58216038c212353f
zoeznm <bomikim046@gmail.com>
2024-10-09 22:36:07 +0900

    모자 라우팅 성공

---
+++ mywishlist/client/src/pages/hat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/hat.css"; 
+import "../scripts/hat.js";
+
+const Hat = () => {
+  return(
+    <h1>
+      여기는 모자페이지 입니다.
+    </h1>
+  );
+};
+
+export default Hat;
