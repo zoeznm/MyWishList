b5ad0e1c18149920a71c9af2b02d0dace739fa44
zoeznm <bomikim046@gmail.com>
2024-10-09 22:35:33 +0900

    하의 라우팅 성공

---
+++ mywishlist/client/src/pages/bottom.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/bottom.css"; 
+import "../scripts/bottom.js";
+
+const Bottom = () => {
+  return(
+    <h1>
+      여기는 바텀페이지 입니다.
+    </h1>
+  );
+};
+
+export default Bottom;
